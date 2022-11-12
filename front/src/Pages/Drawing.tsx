ecd2e588161c39f7254098cfc98fe5dbafe54c91
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2022-11-12 18:55:43 +0200

    expanded drawing page

--- front/src/Pages/Drawing.tsx
+++ front/src/Pages/Drawing.tsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CanvasDraw from 'react-canvas-draw';
+import ColorPickers from '../Components/ColorPickers';
 
-export default function Drawing() {
-  return <div>Drawing</div>;
+export default function Drawing({
+  word,
+  player2,
+}: {
+  word: string;
+  player2: string;
+}) {
+  const [brushColor, setBrushColor] = useState('#000000');
+  return (
+    <div>
+      <div className='header'>
+        <div className='score'>{0}</div>
+        <div className='description'>
+          you are drawing {word.toUpperCase()}
+          <br />
+          for {player2}
+        </div>
+      </div>
+      <CanvasDraw
+        brushColor={brushColor}
+        hideInterface={true}
+        hideGrid={true}
+      />
+      <ColorPickers setBrushColor={setBrushColor} />
+    </div>
+  );
 }

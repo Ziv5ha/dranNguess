d01890736baed5e736b84b7ccee2b390d33d21a1
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2022-11-12 21:44:03 +0200

    expand drawing page

--- front/src/Pages/Drawing.tsx
+++ front/src/Pages/Drawing.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import CanvasDraw from 'react-canvas-draw';
 import ColorPickers from '../Components/ColorPickers';
 
@@ -10,6 +10,16 @@ export default function Drawing({
   player2: string;
 }) {
   const [brushColor, setBrushColor] = useState('#000000');
+  const canvasRef = useRef<CanvasDraw>(null);
+  const undoFunc = () => {
+    canvasRef.current?.undo();
+  };
+  const eraserFunc = () => {
+    canvasRef.current?.clear();
+  };
+  const submitFunc = () => {
+    const drawing = canvasRef.current?.getSaveData();
+  };
   return (
     <div>
       <div className='header'>
@@ -20,7 +30,19 @@ export default function Drawing({
           for {player2}
         </div>
       </div>
+      <div className='tools'>
+        <button className='tool-btn' onClick={undoFunc}>
+          undo
+        </button>
+        <button className='tool-btn' onClick={eraserFunc}>
+          eraser
+        </button>
+        <button className='tool-btn' onClick={submitFunc}>
+          submit
+        </button>
+      </div>
       <CanvasDraw
+        ref={canvasRef}
         brushColor={brushColor}
         hideInterface={true}
         hideGrid={true}
